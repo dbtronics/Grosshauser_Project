3208a8248ee96a3b9985fab8126bae57f5b87c77
Dian Basit <37752863+dbtronics@users.noreply.github.com>
2025-04-04 09:17:18 -0600

    Update battery_level_demo.ino and max and min is calculated based on observed LiPo batteries
    It was tested with 4V LiPo and 3.4V LiPo and v_adc were observed with previous calibration and adjustments to min/max was made according to these adjustments.

--- demo/battery_level_demo/battery_level_demo.ino
+++ demo/battery_level_demo/battery_level_demo.ino
@@ -3,9 +3,9 @@
 #define ADC_MAX 4095.0 // based on 12bit ADC
 #define V_REF 3.3  // ADC reference voltage
 
-// Battery voltage limits after voltage divider
-#define V_ADC_FULL 3.3     // Corresponds to 4.2V battery
-#define V_ADC_EMPTY 2.91   // Corresponds to 3.7V battery
+// Calibrated ADC voltage range
+#define V_ADC_FULL 3.13   // Observed at 4.2V (values calculated based on observed values and max is extrapolated)
+#define V_ADC_EMPTY 2.13  // Estimated at 3.0V (values calcualted based on observed values and min is extrapolated)
 
 void setup() {
   Serial.begin(115200);
@@ -15,12 +15,15 @@ void loop() {
   int adc = analogRead(BATTERY_ADC_PIN);
   float v_adc = (adc / ADC_MAX) * V_REF;
 
-  // Linear mapping of voltage to percentage
+  // Linear mapping of voltage to percentage (reduced down to 3.3V)
   float percent = (v_adc - V_ADC_EMPTY) / (V_ADC_FULL - V_ADC_EMPTY) * 100.0;
   percent = constrain(percent, 0, 100);
 
-  Serial.print("V_ADC: ");
-  Serial.print(v_adc, 3);
+  // Output all key values
+  Serial.print("ADC: ");
+  Serial.print(adc);
+  Serial.print(" | V_ADC: ");
+  Serial.print(v_adc, 3); // this is reduced to 3.3V
   Serial.print(" V | Battery: ");
   Serial.print(percent, 1);
   Serial.println(" %");
